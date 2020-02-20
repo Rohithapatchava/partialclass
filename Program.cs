cca4f663e672a28139bd3aa6fd149ad8548e8afc
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-20 16:25:34 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partialclass
+{
+    partial class c
+    {
+
+    }
+    partial class c
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo.c obj = new demo.c();
+            obj.m();
+        }
+    }
+    namespace demo
+    {
+        class c
+        {
+            public void m()
+            {
+                Console.WriteLine("hai");
+            }
+        }
+    }
+}
